21e30dbc75901515b22d21f2a3c935034c116dd5
karthi071 <karthigsckdm@gmail.com>
2025-08-16 22:09:59 +0530

    Add files via upload
    day1 assignment1

---
+++ day1/EmployeeDetails.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		        String name="karthi";
+		        int age= 30;
+				double salery=55000.75;
+				char Gender='M';
+				boolean acceptterms=true;
+				float rating=4.5f;
+				long mobilenumber=9876543210L;
+				int pincode=638702;
+				byte luckynumber=7;
+				System.out.println("Name:"+name+"\n"+
+					    "Age:"+age+"\n"+
+						"Salery:"+salery+"\n"+
+					    "Gender:"+Gender+"\n"+
+						"Acceptterms:"+acceptterms+"\n"+
+					    "Rating:"+rating+"\n"+
+						"Mobilenumber:"+mobilenumber+"\n"+
+					    "Pincode:"+pincode+"\n"+
+					    "Lucky number:"+luckynumber);
+	}
+
+}
