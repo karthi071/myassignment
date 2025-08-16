2aa493d42875e6e2076b20ed3ffbfc2a8ed4df2b
karthi071 <karthigsckdm@gmail.com>
2025-08-16 20:30:15 +0530

    Add files via upload
    day1

---
+++ day1/HelloTestleaf.java
@@ -0,0 +1,11 @@
+package week1.day1;
+
+public class HelloTestleaf {
+
+	public static void main(String[] args) {
+		System.out.println("Wellcome To Testleaf");
+		// TODO Auto-generated method stub
+
+	}
+
+}
